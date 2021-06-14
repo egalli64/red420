50dfd1a2515e1a13f0425590a9234bb27bd447e2
Emanuele Galli <egalli64@gmail.com>
2021-06-14 12:04:10 +0200

    login stub

---
+++ src/main/java/red/Login.java
@@ -0,0 +1,17 @@
+package red;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+@SuppressWarnings("serial")
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
